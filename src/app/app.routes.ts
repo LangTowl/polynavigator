74033c2cd2a33d116a825ad65d079b4dd1dfdf24
Lang Towl <langtowl@gmail.com>
2025-02-21 07:51:16 -0500

    Optimized routing import statement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { getPlatform } from '../app/services/breakpoint/breakpoint.util';
+import { getPlatform } from './services/breakpoint/breakpoint.util';
 import { DesktopManagerComponent} from './views/desktop-manager/desktop-manager.component';
 import { MobileManagerComponent } from './views/mobile-manager/mobile-manager.component';
 
