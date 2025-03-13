f9d6135c45d8daafe916dca460d7f134ce871975
Lukas-k03 <98773707+Lukas-k03@users.noreply.github.com>
2025-03-12 20:06:30 -0400

    03/12/2025 desktop-register page

---
+++ src/app/views/desktop-register-page/desktop-register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-register-page',
+  imports: [],
+  templateUrl: './desktop-register-page.component.html',
+  styleUrl: './desktop-register-page.component.scss'
+})
+export class DesktopRegisterPageComponent {
+
+}
