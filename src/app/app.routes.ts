434b1c0649dc1c9fdaa86bbcb517386bbb6d9a88
Lukas-k03 <98773707+Lukas-k03@users.noreply.github.com>
2025-03-16 21:28:19 -0400

    03/16/2025
    making mobile reg and log

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { getPlatform } from './services/breakpoint/breakpoint.util';
 import { DesktopLoginPageComponent} from './views/desktop-login-page/desktop-login-page.component';
 import { MobileLoginPageComponent } from './views/mobile-login-page/mobile-login-page.component';
 import {DesktopRegisterPageComponent} from './views/desktop-register-page/desktop-register-page.component';
+import {MobileRegisterPageComponent} from './views/mobile-register-page/mobile-register-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
 
   {path: 'login', component: getPlatform() === 'mobile' ? MobileLoginPageComponent : DesktopLoginPageComponent,},
 
-  {path: 'register', component: DesktopRegisterPageComponent,}
+  {path: 'register', component: getPlatform() === 'mobile' ? MobileRegisterPageComponent : DesktopRegisterPageComponent,}
 ];
---
+++ src/app/views/mobile-register-page/mobile-register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-register-page',
+  imports: [],
+  templateUrl: './mobile-register-page.component.html',
+  styleUrl: './mobile-register-page.component.scss'
+})
+export class MobileRegisterPageComponent {
+
+}
