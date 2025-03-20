8646edbea0e00dc60a2cad5be9050859d6c468b7
Lukas-k03 <98773707+Lukas-k03@users.noreply.github.com>
2025-03-19 22:55:09 -0400

    03/19/2025 started map dev for web

--- src/app/views/desktop-map-page/desktop-map-page.component.ts
+++ src/app/views/desktop-map-page/desktop-map-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-desktop-map-page',
-  imports: [],
   templateUrl: './desktop-map-page.component.html',
   styleUrl: './desktop-map-page.component.scss'
 })
