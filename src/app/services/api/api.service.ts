24d98314f0759950ffe3bef41ba4cb3e7e3a1db9
Lang Towl <langtowl@gmail.com>
2025-04-16 17:04:54 -0400

    Build 2.2 -> uses NGROK tunneling URL

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -17,7 +17,7 @@ export class ApiService {
   http = inject(HttpClient);
 
   // Local vars
-  private url = 'https://d37c-34-122-95-14.ngrok-free.app';
+  private url = 'https://6723-34-60-187-85.ngrok-free.app';
 
   constructor() { }
 
